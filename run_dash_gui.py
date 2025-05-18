d774795be00697410a8e928fa5dc3f2cd42c4f44
Exwhybaba <seyeoyelayo@gmail.com>
2025-05-18 09:25:18 +0100

    DashTools Automatic Push - 05/18/2025 @ 09:25:17

--- run_dash_gui.py
+++
@@ -1,8 +0,0 @@
-try:
-    print("Launching DashTools GUI...")
-    from dashtools.dashboard.dashboard import run_dashboard
-    run_dashboard()
-except Exception as e:
-    print(f"Error: {e}")
-    import traceback
-    traceback.print_exc() 
