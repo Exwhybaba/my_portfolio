d82b3a40b84c52e1a85d8a5a0edfd3ec091188ce
Exwhybaba <seyeoyelayo@gmail.com>
2025-05-18 15:00:08 +0100

    add update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from src.app import app, server
 
+# Make the server object callable for WSGI
+application = server
+
 # This file is used by Render to locate the application
 if __name__ == "__main__":
     app.run_server(debug=False) 
