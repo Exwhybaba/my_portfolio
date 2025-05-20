50aeff25a70d6719fa105938386500fb368d320e
Oyelayo Seye <seyeoyelayo@gmail.com>
2025-05-20 15:07:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -625,4 +625,5 @@ def submit_form(n_clicks, name, email, message):
     )
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=2045)
+    port = int(os.environ.get('PORT', 8050))  # Dynamic port for Render
+    app.run_server(host='0.0.0.0', port=port, debug=False)
